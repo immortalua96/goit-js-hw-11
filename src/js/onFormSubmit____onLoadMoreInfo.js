738151bcbecd8dd5d0a8cf2fcf04a3f28cf26cc6
immortalua96 <romanshvagla@icloud.com>
2023-06-04 19:50:22 +0200

    finish

--- src/js/onFormSubmit____onLoadMoreInfo.js
+++ src/js/onFormSubmit____onLoadMoreInfo.js
@@ -54,7 +54,8 @@ export async function onLoadMoreInfo(evt) {
     renderImageList(response.hits);
     gallerySimpleLightbox.refresh();
 
-    if (response.totalHits - 40 < 40) {
+    if (response.totalHits / 40 < pageNr) {
+      console.log(response.totalHits - 40);
       refs.btnLoadMore.style.display = 'none';
       Notiflix.Notify.info(
         "We're sorry, but you've reached the end of search results."
